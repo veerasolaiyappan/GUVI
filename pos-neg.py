b5d62227972cc9e8658868632a796253a5c1e8a5
veerasolaiyappan <veerasolaiyappan@gmail.com>
2018-08-11 08:32:35 +0530

    postive or negative

---
+++ pos-neg.py
@@ -0,0 +1,8 @@
+#check the given integer is positive or negative
+
+n = int(input("Enter the integer value:"))
+
+if(n>0):
+    print(" %d is POSTIVE INTEGER"%(n))
+else:
+    print("%d is NAGETIVE INTEGER"%(n))
