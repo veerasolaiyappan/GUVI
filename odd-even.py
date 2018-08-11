dfd2d0423fb787750bceb976d8671722d1fc52bc
veerasolaiyappan <37221548+veerasolaiyappan@users.noreply.github.com>
2018-08-11 08:39:55 +0530

    Update odd-even.py

--- odd-even.py
+++ odd-even.py
@@ -2,7 +2,10 @@
 
 n = int(input("Enter the any integer value: "))
 
-if(n%2==0):
-    print("%d is even number "%(n))
+if(n>0):
+    if(n%2==0):
+        print("%d is even number "%(n))
+    else:
+        print("%d is odd number "%(n))
 else:
-    print("%d is odd number "%(n))
+    print("invalid input")
